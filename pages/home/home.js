7db220bad9b5ef0a440a840be7fe10254edb08c8
zmlxsdm <zmlxsdm@163.com>
2020-02-16 15:28:08 +0800

    第一天学习

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,26 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:"Codezml",
+    age:18,
+    students:[
+      { id: 1, name: '张三', age: 18},
+      { id: 2, name: '王五', age: 19 },
+      { id: 3, name: '老刘', age: 20 },
+      { id: 4, name: '小七', age: 21 },
+      { id: 5, name: '大巴', age: 22 }
+    ],
+    count:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    // console.log("按钮发生了点击")
+    // this.setDate()
+    this.setData({
+      count:this.data.count+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      count:this.data.count-1
+    })
   }
 })
