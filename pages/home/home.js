65dd3f992ad346431444df2978fccecccda7be3e
zmlxsdm <zmlxsdm@163.com>
2020-02-17 21:32:03 +0800

    第二天

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,61 +6,33 @@ Page({
    */
   data: {
 
+  },//按钮获取授权
+  handleGetUserInfo(event){
+    console.log(event)
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // -------------------------1、监听页面的生命周期-----------
+  //页面被加载出来时，回调函数
+  onLoad(){
+    console.log('onLoad')
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success:(res)=>{
+        console.log(res)
+      }
+    })
+  },//页面初次渲染完成时
+  onReady(){
+    console.log('onReady')
+  },//页面显示出来时
+  onShow(){
+    console.log('onShow')
+  },//当页面隐藏起来时
+  onHide(){
+    console.log('onHide')
+  },//页面发生跳转，新页面销毁，返回首页时执行回调函数
+  onUnload(){
+    console.log('onUnload')
   }
+
+  
 })
