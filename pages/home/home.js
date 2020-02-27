67a81624b518396f0f61b28286f1ccc1a86c7ffd
zmlxsdm <zmlxsdm@163.com>
2020-02-27 18:01:02 +0800

    网络请求封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,7 @@
 // pages/home/home.js
+
+import request from '../../servce/network.js'
+
 Page({
 
   /**
@@ -12,55 +15,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //原生
+      // wx.request({
+      //   url: 'http://123.207.32.32:8000/recommend',
+      //   success:function(res){
+      //       console.log(res)
+      //   }
+      // }),
+      // wx.request({
+      //   url: 'http://httpbin.org/post',
+      //   method:'post',
+      //   data:{
+      //     name:'zml',
+      //     age:18
+      //   },
+      //   success:function(res){
+      //     console.log(res)
+      //   }
+      // })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      request({
+        url:'http://123.207.32.32:8000/recommend'
+      }).then(res=>{
+        console.log(res)
+      }).catch(err=>{
+        console.log(err)
+      })
   }
 })
